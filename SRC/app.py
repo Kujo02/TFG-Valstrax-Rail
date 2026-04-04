77ee0f6936885bb1ed33226ff0526ff1dfc68d77
Kujo02 <joseppastor2288@gmail.com>
2026-04-04 21:48:43 +0200

    He creado mi primera ruta con blueprints.

--- SRC/app.py
+++ SRC/app.py
@@ -2,11 +2,16 @@ from flask import Flask, render_template, request
 import os
 from config import config
 
+
+from routes.main import main
+
+
+
 app = Flask(__name__)
 
 app.config.from_object(config['development'])
 
-
+app.register_blueprint(main)
 
 if __name__ == '__main__':
     app.run(debug=True)
